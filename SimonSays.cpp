6a3dac12647cc2f17ab28466b2bc8fe28dd5a6eb
smyousuf14 <44485548+smyousuf14@users.noreply.github.com>
2019-06-24 17:54:52 -0400

    Create SimonSays.cpp

---
+++ SimonSays.cpp
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) 
+	{
+		// Local Variables
+		int commandNumbers;
+		String[] commands;
+		int indexNumber;
+		
+	    Scanner scan = new Scanner(System.in);
+	    
+	    commandNumbers = scan.nextInt();
+	    
+	    // Set value to the commands
+	    commands = new String[commandNumbers];
+	    
+	    
+	    
+	}
+}
