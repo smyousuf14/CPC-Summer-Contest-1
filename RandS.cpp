6dd69dab35805bb0f8ecc5d8868b92e99f778bd2
smyousuf14 <44485548+smyousuf14@users.noreply.github.com>
2019-06-24 17:23:58 -0400

    Create RandS.cpp

---
+++ RandS.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Local Variables
+    int r1;
+    int r2;
+    int s;
+    
+    cin >> r1 >> s;
+    
+    // Determine r2
+    r2 = (2 * s) - r1;
+    
+    cout << r2;
+}
