812667d7f61a4b3bcf7185baf77274d84a26d5e6
smyousuf14 <44485548+smyousuf14@users.noreply.github.com>
2019-06-27 12:14:59 -0400

    Update SimonSays.cpp

--- SimonSays.cpp
+++ SimonSays.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string.h>
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -8,15 +9,33 @@ int main()
     // Local Variable list
     int numberOfCommands;
     int indexNumber;
+    string checkValue;
     
     cin >> numberOfCommands;
     
     string commands[numberOfCommands];
     
+    // Clear the input and move to the next line.
+    cin.clear();
+    cin.ignore(10000, '\n');
+    
     // Now accept all the commands.
     for(indexNumber = 0; indexNumber < numberOfCommands; indexNumber++)
     {
         getline(cin, commands[indexNumber]);
-        cout << indexNumber<< endl;
     }
+    
+    // Now check each input and perform appropriate action.
+    for(indexNumber = 0; indexNumber < numberOfCommands; indexNumber++)
+    {
+        checkValue = commands[indexNumber].substr(0, 9);
+        
+        // Check the value.
+        if(checkValue == "Simon says")
+        {
+            cout << commands[indexNumber].substr(10, commands[indexNumber].length());
+        }
+    }
+    
+    return 0;
 }
